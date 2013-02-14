a20fd75ddf99176252499b9e2134c2d0aa3663f2
alex <alex@alex-HP-Pavilion-dv5-Notebook-PC.(none)>
2013-02-15 00:48:24 +0400

    bug fix dfa

--- lab1/code/determinization_algorithm.py
+++ lab1/code/determinization_algorithm.py
@@ -11,50 +11,30 @@ class DeterminizationAlgorithm:
 
     def _build_subsets(self):
         last_state = 0
-        gen_states = {}
-
+        generalized_states = {}
         transition_table ={}
 
         closure = self._closure([self.nfa.initial_state], self.nfa.transition_table)
-        gen_states = {last_state: set(closure)}
+        generalized_states = {last_state: set(closure)}
         last_state += 1
-        print "closure:", closure
-        print "gen states:", gen_states
 
         d_states = [set(closure)]
-        print "d_states:", d_states
         unmarked = [set(closure)]
-        print "unmarked:", unmarked
 
         while unmarked:
-            print "==============================="
             T = unmarked.pop()
-            print "t:", T
-            print "closure:", closure
-            print "unmarked:", unmarked
-            print "gen_states:", gen_states
-            print "************************************************"
             transitions = {}
             for a in self.alphabet:   
-                print "a:", a        
                 mv = self._move(list(T), a, self.nfa.transition_table)
-                print "mv:", mv
                 U = set(self._closure(mv, self.nfa.transition_table))
-                print "u:", U
                 if U:
-                    print "d_states:", d_states
                     if U not in d_states:
                         d_states.append(U)
-                        gen_states[last_state] = U
+                        generalized_states[last_state] = U
                         last_state += 1
                         unmarked.append(U)
-
-                    print "d_states:", d_states
-                    print "unmarked:", unmarked
-                    transitions.update({a: [self.get_key_by_value(gen_states, U)]})
-                    print "Dtran:", transition_table
-                    print "************************************************"
-            transition_table[self.get_key_by_value(gen_states, T)] = transitions   
+                    transitions.update({a: [self.get_key_by_value(generalized_states, U)]})
+            transition_table[self.get_key_by_value(generalized_states, T)] = transitions   
         return FA(transition_table, self.nfa.initial_state, self.nfa.final_state)
 
     def get_key_by_value(self, dictionary, value):
@@ -93,7 +73,10 @@ class DeterminizationAlgorithm:
                     result[key] = states
         return result
 
-regexp = "aaabbb"
+#regexp = "aaabbb"
+#regexp = "a(ab)*b"
+#regexp = "a(a|b)*b"
+regexp = "a(a*|b)*b"
 alg1 = TompsonAlgorithm(regexp)
 fa = alg1.buildNFA()
 fa.draw('graph')
