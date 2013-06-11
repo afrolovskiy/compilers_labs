355b5fabfc0a5ee95b0d3c3c2a821f4bc6016e7d
alex <alex@alex-HP-Pavilion-dv5-Notebook-PC.(none)>
2013-06-12 01:51:13 +0400

    temp

--- compiler/lexer.py
+++ compiler/lexer.py
@@ -16,21 +16,23 @@ class MiniJavaLexer:
 		'if': 'IF',
 		'else': 'ELSE',
 		'while': 'WHILE',
-		'System.out.println': 'PRINTLN',
 		'lenght': 'LENGTH',
 		'true': 'TRUE',
 		'false': 'FALSE',
 		'this': 'THIS',
 		'new': 'NEW',
+		'System': 'SYSTEM',
+		'out': 'OUT',
+		'println': 'PRINTLN',
 	}
 
 	tokens = [
 		'LEFT_PARENTHESIS',
 		'RIGHT_PARENTHESIS',
 		'LEFT_BRACKET',
-		'RIGHT_BRACKET'
+		'RIGHT_BRACKET',
 		'LEFT_BRACE',
-		'RIGTH_BRACE',
+		'RIGHT_BRACE',
 		'POINT',
 		'COMMA',
 		'SEMICOLON',
@@ -46,31 +48,31 @@ class MiniJavaLexer:
 	t_LEFT_BRACKET = r'\['
 	t_RIGHT_BRACKET = r'\]'
 	t_LEFT_BRACE = r'{'
-	t_RIGTH_BRACE = r'}'
+	t_RIGHT_BRACE = r'}'
 	t_POINT = r'\.'
 	t_COMMA = r','
 	t_SEMICOLON = r';'
 	t_NOT = r'!'
 	t_ASSIGNMENT = r'='
-	t_BINARY_OPERATOR = r'(&&|<|+|-|\*)'
+	t_BINARY_OPERATOR = r'&&|<|\+|-|\*'
 
-	def t_INTEGER_LITERAL(t):
-		r'([0-9]+)'
+	def t_INTEGER_LITERAL(self, t):
+		r'\d+'
 		t.value = int(t.value)
 		return t
 
-	def t_IDENTIFIER(t):
-		r'([A-Za-z][A-Za-z0-9_]*)'
-		t.type = reserved.get(t.value, 'IDENTIFIER')
+	def t_IDENTIFIER(self, t):
+		r'[A-Za-z][\d\w]*'
+		t.type = self.reserved.get(t.value, 'IDENTIFIER')
 		return t
 
-	def t_newline(t):
+	def t_newline(self, t):
 		r'\n+'
 		t.lexer.lineno += len(t.value)
 
 	t_ignore  = ' \t'
 
-	def t_error(t):
+	def t_error(self, t):
 		print "Illegal character '%s'" % t.value[0]
 		t.lexer.skip(1)
 
@@ -80,31 +82,32 @@ class MiniJavaLexer:
 	def test(self,data):
 		self.lexer.input(data)
 		while True:
-			 tok = lexer.token()
+			 tok = self.lexer.token()
 			 if not tok: break
 			 print tok
 
 
-m = MiniJavaLexer()
-m.build()
-m.test(
-	"""
-	class Factorial {
-		public static void main(String[] a) {
-		    System.out.println(new Fac().ComputeFac(10));
+if __name__ == '__main__':
+	m = MiniJavaLexer()
+	m.build()
+	m.test(
+		"""
+		class Factorial {
+			public static void main(String[] a) {
+				System.out.println(new Fac().ComputeFac(10));
+			}
 		}
-	}
 
-	class Fac {
-		public int ComputeFac(int num) {
-		    int num_aux;
-		if (num < 1)
-			num_aux = 1;
-		else
-			num_aux = num * (this.ComputeFac(num - 1));
-		return num_aux;
+		class Fac {
+			public int ComputeFac(int num) {
+				int num_aux;
+			if (num < 1)
+				num_aux = 1;
+			else
+				num_aux = num * (this.ComputeFac(num - 1));
+			return num_aux;
+			}
 		}
-	}
-	"""
-)
+		"""
+	)
 
---
+++ compiler/syntaxer.py
@@ -0,0 +1,195 @@
+import ply.yacc as yacc
+from lexer import MiniJavaLexer
+
+
+tokens = MiniJavaLexer.tokens
+lexer = MiniJavaLexer()
+start = 'goal'
+
+def p_goal(p):
+	'goal : main_class class_declaration_list'
+
+def p_class_declaration_list(p):
+	'''class_declaration_list : 
+									   | class_declaration class_declaration_list
+	'''
+
+def p_main_class(p):
+	'''
+	main_class : 	CLASS IDENTIFIER LEFT_BRACE PUBLIC STATIC VOID MAIN LEFT_PARENTHESIS STRING LEFT_BRACKET RIGHT_BRACKET IDENTIFIER RIGHT_PARENTHESIS LEFT_BRACE 	statement RIGHT_BRACE RIGHT_BRACE
+	'''
+
+def p_class_declaration(p):
+	'''
+	class_declaration : CLASS IDENTIFIER extends_list LEFT_BRACE var_declaration_list method_declaration_list 	RIGHT_BRACE
+	'''
+
+def p_extends_list(p):
+	'''
+	extends_list : 
+				 | EXTENDS IDENTIFIER
+	'''
+
+def p_var_declaration_list(p):
+	'''
+	var_declaration_list : 
+								 | var_declaration var_declaration_list
+	'''
+
+def p_method_delaration_list(p):
+	'''
+	method_declaration_list :
+									   | method_declaration method_declaration_list
+	'''
+
+
+def p_var_declaration(p):
+	'var_declaration : type IDENTIFIER SEMICOLON'
+
+def p_method_declaration(p):
+	'''
+	method_declaration : PUBLIC type IDENTIFIER LEFT_PARENTHESIS params_list RIGHT_PARENTHESIS LEFT_BRACE var_declaration_list statement_list RETURN expression SEMICOLON RIGHT_BRACE
+	'''
+
+def p_params_list(p):
+	'''
+	params_list : type IDENTIFIER
+					  | type IDENTIFIER COMMA params_list
+	'''
+
+def p_statement_list(p):
+	"""
+	statement_list : 
+						  | statement statement_list
+    """
+
+def p_type(p):
+	'''type : int_array_type 
+		   	 | boolean_type 
+			 | int_type 
+			 | identifier_type
+	'''
+
+def p_int_array_type(p):
+	'int_array_type : INT LEFT_BRACKET RIGHT_BRACKET'
+
+def p_boolean_type(p):
+	'boolean_type : BOOLEAN'
+
+def p_int_type(p):
+	'int_type : INT'
+
+def p_identifier_type(p):
+	'identifier_type : IDENTIFIER'
+
+def p_statement(p):
+	'''statement : block_statement 
+					  | if_statement 
+					  | while_statement 
+					  | print_statement 
+					  | 	assignment_statement 
+					  | array_element_assignment_statement'''
+
+def p_block_statement(p):
+	'block_statement : LEFT_BRACE statement_list RIGHT_BRACE'
+
+def p_if_statement(p):
+	'if_statement : IF LEFT_PARENTHESIS expression RIGHT_PARENTHESIS statement ELSE statement'
+
+def p_while_statement(p):
+	'while_statement : WHILE LEFT_PARENTHESIS expression RIGHT_PARENTHESIS statement'
+
+def p_print_statement(p):
+	'print_statement : SYSTEM POINT OUT POINT PRINTLN LEFT_PARENTHESIS expression RIGHT_PARENTHESIS SEMICOLON'
+
+def p_assignment_statement(p):
+	'assignment_statement : IDENTIFIER ASSIGNMENT expression SEMICOLON'
+
+def p_array_element_assignment_statement(p):
+	'array_element_assignment_statement : IDENTIFIER LEFT_BRACKET expression RIGHT_BRACKET ASSIGNMENT expression SEMICOLON'
+
+def p_expression(p):
+	'''expression : operator_expression 
+					   | array_element_expression 
+					   | length_expression 
+					   | call_expression
+					   | integer_literal_expression 
+					   | true_expression 
+					   | false_expression 
+					   | identifier_expression 
+					   | this_expression 
+					   | new_array_expression 
+					   |  new_expression 
+					   | not_expression 
+					   | parenthesis_expression'''
+
+def p_operator_expression(p):
+	'operator_expression : expression BINARY_OPERATOR expression'
+
+def p_array_element_expression(p):
+	'array_element_expression : expression LEFT_BRACKET expression RIGHT_BRACKET'
+
+def p_length_expression(p):
+	'length_expression : expression POINT LENGTH'
+
+def p_call_expression(p):
+	'call_expression : expression POINT IDENTIFIER LEFT_PARENTHESIS expression_list RIGHT_PARENTHESIS'
+
+def p_expression_list(p):
+	'''expression_list : 
+							| expression COMMA expression_list
+	'''
+
+def p_integer_literal_expression(p):
+	'integer_literal_expression : INTEGER_LITERAL'
+
+def p_true_expression(p):
+	'true_expression : TRUE'
+
+def p_false_expression(p):
+	'false_expression : FALSE'
+
+def p_identifier_expression(p):
+	'identifier_expression : IDENTIFIER'
+
+def p_this_expression(o):
+	'this_expression : THIS'
+
+def p_new_array_expression(p):
+	'new_array_expression : NEW INT LEFT_BRACKET expression RIGHT_BRACKET'
+
+def p_new_expression(p):
+	'new_expression : NEW IDENTIFIER LEFT_PARENTHESIS RIGHT_PARENTHESIS'
+
+def p_not_expression(p):
+	'not_expression : NOT expression'
+
+def p_parenthesis_expression(p):
+	'parenthesis_expression : LEFT_PARENTHESIS expression RIGHT_PARENTHESIS'	
+
+def p_error(p):
+	print "Syntax error in input!"
+
+
+if __name__ == '__main__':
+	s = """
+		class Factorial {
+			public static void main(String[] a) {
+				System.out.println(new Fac().ComputeFac(10));
+			}
+		}
+
+		class Fac {
+			public int ComputeFac(int num) {
+				int num_aux;
+			if (num < 1)
+				num_aux = 1;
+			else
+				num_aux = num * (this.ComputeFac(num - 1));
+			return num_aux;
+			}
+		}
+	"""
+	parser = yacc.yacc()
+	result = parser.parse(s)
+	print result
